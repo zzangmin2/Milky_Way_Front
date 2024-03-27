af03dbbf6a2fbf47fc7d03ab33aa8c5da21250ed
ktg <ktg5679@gmail.com>
2024-03-27 15:04:10 +0900

    feat: signup페이지 설정 및 recoil세팅

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -5,8 +5,9 @@ export const StyledButton = styled.button`
   padding: 20px;
   margin-bottom: 20px;
   font-size: 1rem;
+  font-weight: bold;
   border: none;
   border-radius: 10px;
-  background-color: #133488;
+  background-color: ${(props) => props.color || "#133488"};
   color: #fff;
 `;
