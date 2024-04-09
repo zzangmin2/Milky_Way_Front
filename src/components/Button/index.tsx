fd5a68f785c5627136280e6ce43686ea4e53f0af
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2024-04-09 18:10:06 +0900

    feat: button, input 컴포넌트 수정

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { StyledButton } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   text?: string;
   color?: string;
   url?: string;
   onClick?: () => void; // 컴포넌트 이벤트처리
+  buttonState?: string; // 'active' -> 버튼 활성화, 'Inactive' -> 버튼 비활성화
 }
 
-const Button: React.FC<Props> = ({ text, color, onClick }) => {
+const Button: React.FC<Props> = ({ text, color, onClick, buttonState }) => {
   return (
     <StyledButton color={color} onClick={onClick}>
       {text}
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,35 @@
-import { FC, ChangeEvent } from "react";
+import { FC, ChangeEvent, useEffect } from "react";
 import { StyledInput } from "./styles";
 
 interface Props {
-  placeholder: string;
+  placeholder?: string;
   name?: string;
   value: string;
   setValue: (value: string) => void;
+  inputType?: string;
+  inputState?: string;
 }
 
-const Input: FC<Props> = ({ placeholder, name, value, setValue }) => {
+const Input: FC<Props> = ({
+  placeholder,
+  name,
+  value,
+  setValue,
+  inputType,
+}) => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
+  useEffect(() => {
+    if (!inputType) {
+      inputType = "text";
+    }
+  }, []);
+
   return (
     <StyledInput
-      type="text"
+      type={inputType}
       placeholder={placeholder}
       onChange={handleChange}
       value={value}
