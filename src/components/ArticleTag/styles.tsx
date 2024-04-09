462d1883dfcd92312808096944a54b98e2a02913
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2024-04-09 18:09:38 +0900

    feat: study -> article 파일명 수정

--- src/components/ArticleTag/styles.tsx
+++ src/components/ArticleTag/styles.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledStudyProjectTag = styled.div`
+export const StyledArticleProjectTag = styled.div`
   padding: 2px 10px;
   text-align: center;
   background-color: #ff9078;
