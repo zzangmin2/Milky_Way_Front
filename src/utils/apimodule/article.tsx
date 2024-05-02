206b14c6d01937b7369da2061c86f536c02b981d
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2024-05-02 14:28:06 +0900

    fix: 타입 수정 및 api수정

--- src/utils/apimodule/article.tsx
+++ src/utils/apimodule/article.tsx
@@ -21,28 +21,26 @@ const sendNewArticle = async (newArticleData: Article) => {
     articleType,
     articleApply,
     articleMentorNeeded,
-    articlementorTag,
+    articleMentorTag,
+    articleStartDay,
     articleEndDay,
     articleTitle,
     articleContent,
   } = newArticleData;
   try {
-    const response = await api.post("http://localhost:3000/currentArticle", {
+    await api.post("http://localhost:3000/currentArticle", {
       articleMemberId: articleMemberId,
       articleType: articleType,
       articleApply: articleApply,
       findMentor: articleMentorNeeded,
-      mentorTag: articlementorTag,
+      articleMentorTag: articleMentorTag,
+      articleStartDay: articleStartDay,
       articleEndDay: articleEndDay,
       articleTitle: articleTitle,
       articleContent: articleContent,
     });
 
-    if (response.data.success) {
-      return { success: true };
-    } else {
-      return { success: false };
-    }
+    return { success: true };
   } catch (error) {
     console.error("error:", error);
     return { success: false, error: "error" };
